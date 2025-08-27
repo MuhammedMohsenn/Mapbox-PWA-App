8d2a8ad958ea74e1094c37a72cbc0b80a39c804e
MuhammedMohsenn <muhammedmohsen8695@gmail.com>
2025-08-27 19:11:04 +0300

    Initial app setup with PWA and mapbox

---
+++ app/manifest.js
@@ -0,0 +1,23 @@
+export default function manifest() {
+  return {
+    name: "Mapbox PWA App",
+  short_name: "MapboxPWA",
+  description: "A Progressive Web App with Mapbox integration",
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#000000',
+    icons: [
+       {
+        src: '/web-app-manifest-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/web-app-manifest-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  }
+}
